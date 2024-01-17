be308cc62bc3dd231d8a2d221a869e25c5694353
Mattia <mattia.valerio2002@gmail.com>
2024-01-17 14:08:45 +0100

    Add @melt-ui/pp dependency and update Svelte configuration

---
+++ src/Components/ConfirmSend.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let open = false;
+	export let msg = '';
+</script>
+
+{#if open}
+	<div
+		class="h-full w-full bg-black bg-opacity-60 flex items-center justify-center absolute top-0 bottom-0 left-0 right-0"
+	>
+		<div
+			class="bg-slate-500 h-72 w-full sm:w-full md:w-[50%] lg:w-[25%] xl:w-[25%] m-12 border rounded-lg flex items-center justify-around flex-col px-4"
+		>
+			<h1 class="text-4xl text-center text-slate-100">{msg}</h1>
+			<div class="flex justify-center">
+				<button
+					on:click={() => (open = !open)}
+					class="border shadow-slate-700 shadow-xl px-3 py-1 rounded-md bg-slate-500 text-2xl text-slate-100"
+				>
+					Close
+				</button>
+			</div>
+		</div>
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,34 @@
 <script>
+	import ConfirmPopup from '../Components/ConfirmSend.svelte';
+	let confirm = false;
+
 	function scrolldiv() {
 		var elem = document.getElementById('works-page');
 
 		elem !== null ? elem.scrollIntoView({ block: 'end', behavior: 'smooth' }) : null;
+	}
+
+	function scrolldiv2() {
+		var elem = document.getElementById('works-list');
+
+		if (elem !== null) {
+			var topPos = elem.offsetTop;
+			elem.scrollTo({
+				top: 0,
+				left: topPos,
+				behavior: 'smooth'
+			});
+
+			console.log(elem.offsetTop);
+		}
 	}
 </script>
 
 <svelte:head>
 	<title>MattiaValerio</title>
 </svelte:head>
+
+<ConfirmPopup open={confirm} msg="Thak you !" />
 
 <div class="h-full w-full bg-slate-500 overflow-auto">
 	<div id="landing-page" class="section active flex flex-col items-center justify-center h-full">
@@ -44,12 +64,18 @@
 		id="works-page"
 		class="flex flex-col gap-4 items-center justify-around h-full w-full py-2 bg-slate-500 overflow-y-auto"
 	>
-		<h1 class="text-6xl font-bold text-center text-slate-100">My works</h1>
+		<div class="w-full flex items-center justify-around">
+			<button class="border px-2 rounded-md font-bold text-slate-100">Left</button>
+			<h1 class="text-4xl md:lg:text-6xl font-bold text-center text-slate-100">My works</h1>
+			<button class="border px-2 rounded-md font-bold text-slate-100" on:click={() => scrolldiv2()}
+				>Right</button
+			>
+		</div>
 
-		<div class="flex overflow-x-scroll h-full w-full gap-10 p-3">
+		<div id="works-list" class="flex overflow-x-scroll h-full w-full gap-10 p-3">
 			<div
 				id="work-1"
-				class="flex flex-shrink-0 flex-col items-center h-full w-full bg-slate-100 justify-center gap-5 rounded-md"
+				class="flex flex-shrink-0 flex-col items-center h-full w-full md:w-[45%] bg-slate-100 justify-center gap-5 rounded-md"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-700">Lorem Ipsum</h2>
 				<p class="text-center text-slate-700">
@@ -59,7 +85,7 @@
 
 			<div
 				id="work-2"
-				class="flex border flex-shrink-0 w-full flex-col items-center justify-center gap-5"
+				class="flex md:w-[50%] border flex-shrink-0 w-full flex-col items-center justify-center gap-5"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-100">Lorem Ipsum</h2>
 				<p class="text-center text-slate-100">
@@ -69,7 +95,7 @@
 
 			<div
 				id="work-3"
-				class="flex border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
+				class="flex md:w-[50%] border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-100">Lorem Ipsum</h2>
 				<p class="text-center text-slate-100">
@@ -79,7 +105,7 @@
 
 			<div
 				id="work-4"
-				class="flex border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
+				class="flex md:w-[50%] border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-100">Lorem Ipsum</h2>
 				<p class="text-center text-slate-100">
@@ -89,7 +115,7 @@
 
 			<div
 				id="work-5"
-				class="flex border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
+				class="flex md:w-[50%] border flex-col flex-shrink-0 w-full items-center justify-center gap-5"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-100">Lorem Ipsum</h2>
 				<p class="text-center text-slate-100">
@@ -99,7 +125,7 @@
 
 			<div
 				id="work-6"
-				class="flex border flex-col items-center flex-shrink-0 w-full justify-center gap-5"
+				class="flex md:w-[50%] border flex-col items-center flex-shrink-0 w-full justify-center gap-5"
 			>
 				<h2 class="text-2xl font-bold text-center text-slate-100">Lorem Ipsum</h2>
 				<p class="text-center text-slate-100">
@@ -118,6 +144,7 @@
 			<div class="w-full flex justify-center">
 				<button
 					class="text-white py-3 border shadow-slate-700 shadow-xl flex justify-center gap-1 rounded-md w-[40%] font-bold"
+					on:click={() => (confirm = !confirm)}
 				>
 					<p>Send Message</p>
 					<svg
